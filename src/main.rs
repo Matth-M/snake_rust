261aff2dc4e841a639b592f23560035e3a125b3f
Matth-M <matthieu.matsima@gmail.com>
2023-08-11 01:55:19 +0200

    Draw first snake cell

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,26 @@ use ggez::graphics::{self, Canvas, Color, DrawParam, Quad};
 use ggez::{conf, event, Context, ContextBuilder, GameError, GameResult};
 
 const CELL_SIZE: f32 = 20.;
-struct State {}
+struct State {
+    snake: Snake,
+}
+
+struct Snake {
+    body: Vec<BodyCell>,
+    direction: Direction,
+}
+
+struct BodyCell {
+    x: u32,
+    y: u32,
+}
+
+enum Direction {
+    Left,
+    Right,
+    Up,
+    Down,
+}
 
 impl ggez::event::EventHandler<GameError> for State {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
@@ -17,18 +36,13 @@ impl ggez::event::EventHandler<GameError> for State {
 }
 
 impl State {
-    fn draw_snake(&mut self, canvas: &mut Canvas, ctx: &Context) {
-        self.draw_cell(canvas, ctx, 5, 30, Color::GREEN);
+    fn draw_snake(&self, canvas: &mut Canvas, ctx: &Context) {
+        for cell in &self.snake.body {
+            self.draw_cell(canvas, ctx, cell.x, cell.y, Color::GREEN);
+        }
     }
 
-    fn draw_cell(
-        &mut self,
-        canvas: &mut Canvas,
-        ctx: &Context,
-        row: u32,
-        column: u32,
-        color: Color,
-    ) {
+    fn draw_cell(&self, canvas: &mut Canvas, ctx: &Context, row: u32, column: u32, color: Color) {
         let (window_width, window_height) = ctx.gfx.drawable_size();
         let grid_width_in_cells = (window_width / CELL_SIZE) as u32;
         let grid_height_in_cells = (window_height / CELL_SIZE) as u32;
@@ -43,18 +57,15 @@ impl State {
                     .scale([CELL_SIZE, CELL_SIZE]),
             )
         }
-        canvas.draw(
-            &Quad {},
-            DrawParam::new()
-                .dest([row as f32 * CELL_SIZE, column as f32 * CELL_SIZE])
-                .color(Color::WHITE)
-                .scale([CELL_SIZE, CELL_SIZE]),
-        );
     }
 }
 
 fn main() {
-    let state = State {};
+    let snake = Snake {
+        body: vec![BodyCell { x: 20, y: 5 }],
+        direction: Direction::Right,
+    };
+    let state = State { snake };
     let c = conf::Conf::new();
     let (ctx, event_loop) = ContextBuilder::new("hello_ggez", "matth")
         .default_conf(c)
