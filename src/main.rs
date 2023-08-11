d0b37c41ec6f7500d374cc61a835e9d681b3f2aa
Matth-M <matthieu.matsima@gmail.com>
2023-08-11 20:03:01 +0200

    Add function to draw a cell on the grid

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,53 @@
-use ggez::graphics::{self, Color, DrawParam, Quad};
+use ggez::graphics::{self, Canvas, Color, DrawParam, Quad};
 use ggez::{conf, event, Context, ContextBuilder, GameError, GameResult};
 
-const CELL_SIZE: f32 = 50.;
+const CELL_SIZE: f32 = 20.;
 struct State {}
 
 impl ggez::event::EventHandler<GameError> for State {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::BLACK);
-
-        self.draw_snake(&mut canvas);
-
+        self.draw_snake(&mut canvas, ctx);
         canvas.finish(ctx)
     }
 }
 
 impl State {
-    fn draw_snake(&mut self, canvas: &mut graphics::Canvas) {
-        let snake = Quad {};
+    fn draw_snake(&mut self, canvas: &mut Canvas, ctx: &Context) {
+        self.draw_cell(canvas, ctx, 5, 30, Color::GREEN);
+    }
+
+    fn draw_cell(
+        &mut self,
+        canvas: &mut Canvas,
+        ctx: &Context,
+        row: u32,
+        column: u32,
+        color: Color,
+    ) {
+        let (window_width, window_height) = ctx.gfx.drawable_size();
+        let grid_width_in_cells = (window_width / CELL_SIZE) as u32;
+        let grid_height_in_cells = (window_height / CELL_SIZE) as u32;
+        let invalid_position =
+            row < 1 || row > grid_width_in_cells || column < 1 || column > grid_height_in_cells;
+        if !invalid_position {
+            canvas.draw(
+                &Quad {},
+                DrawParam::new()
+                    .dest([column as f32 * CELL_SIZE, row as f32 * CELL_SIZE])
+                    .color(color)
+                    .scale([CELL_SIZE, CELL_SIZE]),
+            )
+        }
         canvas.draw(
-            &snake,
+            &Quad {},
             DrawParam::new()
-                .dest([150., 150.])
-                .color(Color::GREEN)
+                .dest([row as f32 * CELL_SIZE, column as f32 * CELL_SIZE])
+                .color(Color::WHITE)
                 .scale([CELL_SIZE, CELL_SIZE]),
         );
     }
@@ -40,4 +62,3 @@ fn main() {
         .expect("Could not create context!");
     event::run(ctx, event_loop, state);
 }
-
